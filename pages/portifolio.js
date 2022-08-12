6134222af3aebd4fbffc4834fe7ad9e825fd48f2
Xafixav <xafiworld@gmail.com>
2022-08-11 22:43:29 -0300

    :white_check_mark: implements githubAPI

--- pages/portifolio.js
+++ pages/portifolio.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react';
 import { Header } from '../components/Header';
+import { getRepositories } from '../components/services/fetchAPI';
 
 export default function Portifolio() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    getRepositories('xafixav').then((response) => setRepositories(response));
+  }, []);
+
+  const generateProjects = () => {    
+    return repositories.map((project, index) => (
+      <div key={index}>
+        <h2>{project.name}</h2>
+        {project?.description ? <h4>{project.description}</h4> : null}
+        <a href={project.svn_url}>Ir para o Repositorio</a>
+      </div>
+    ));
+  };  
+
   return (
-    Header()
+    <div> 
+      {Header()}
+      {repositories.length !== 0 ? generateProjects() : null}
+    </div>
   );
 }
