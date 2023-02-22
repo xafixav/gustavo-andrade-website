b64dad1e8a541f799c4808c8ef9c1fa900a5411b
Xafixav <xafiworld@gmail.com>
2023-02-22 07:03:49 -0300

    :white_check_mark: add bootstrap to the entire app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    typeof document !== 'undefined' ? require('bootstrap/dist/js/bootstrap') : null;
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
 
